699f8adf6182549e1be799055a962b51a7ff2d2f
jianye432 <jianye432@gmail.com>
2026-05-24 21:16:30 +0800

    2

--- swift-api-examples/SherpaOnnx-Bridging-Header.h
+++ swift-api-examples/SherpaOnnx-Bridging-Header.h
@@ -4,6 +4,6 @@
 #ifndef SWIFT_API_EXAMPLES_SHERPAONNX_BRIDGING_HEADER_H_
 #define SWIFT_API_EXAMPLES_SHERPAONNX_BRIDGING_HEADER_H_
 
-#import "sherpa-onnx/c-api/c-api.h"
+#import "../sherpa-onnx/c-api/c-api.h"
 
 #endif  // SWIFT_API_EXAMPLES_SHERPAONNX_BRIDGING_HEADER_H_
